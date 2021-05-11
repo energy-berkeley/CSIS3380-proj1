fde97ca687bd0373ea95ebf0d55399d7dc0d18f1
Energy <y1233m@yahoo.com.hk>
2021-05-10 17:02:45 -0700

    finished

--- a_random_quote_slide-show-start/a_random_quote_slide-show-start/js/script.js
+++ a_random_quote_slide-show-start/a_random_quote_slide-show-start/js/script.js
@@ -10,19 +10,107 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+let quotes = [
+  {
+    "quote": "You have no responsibility to live up to what other people think you ought to accomplish. I have no responsibility to be like they expect me to be. It's their mistake, not my failing.",
+    "source": "Richard P. Feynman",
+    "citation": "Surely You're Joking, Mr. Feynman!",
+    "year": "1985"
+  },
+  {
+    "quote": "All the time you're saying to yourself, 'I could do that, but I won't,' — which is just another way of saying that you can't.",
+    "source": "Richard P. Feynman",
+    "citation": "Surely You're Joking, Mr. Feynman!",
+    "year": "1985"
+  },
+  {
+    "quote": "So I have just one wish for you – the good luck to be somewhere where you are free to maintain the kind of integrity I have described, and where you do not feel forced by a need to maintain your position in the organization, or financial support, or so on, to lose your integrity. May you have that freedom.",
+    "source": "Richard P. Feynman",
+    "citation": "Surely You're Joking, Mr. Feynman!",
+    "year": "1985"
+  },
+  {
+    "quote": "I couldn't claim that I was smarter than sixty-five other guys--but the average of sixty-five other guys, certainly!",
+    "source": "Richard P. Feynman",
+    "citation": "Surely You're Joking, Mr. Feynman!",
+    "year": "1985"
+  },
+  {
+    "quote": "Of course, you only live one life, and you make all your mistakes, and learn what not to do, and that's the end of you.",
+    "source": "Richard P. Feynman",
+    "citation": "Surely You're Joking, Mr. Feynman!",
+    "year": "1985"
+  }
+]
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote(){
 
+  // 1. Create a variable that generates a random number
+  // between zero and the last index in the `quotes` array
+
+  let randNum = Math.floor(Math.random() * 4.999);
+
+  // 2. Use the random number variable and bracket notation
+  // to grab a random object from the `quotes` array, and
+  // store it in a variable
+
+  let randEntry = quotes[randNum];
+
+  // 3. Return the variable storing the random quote object
+
+  return randEntry;
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote(){
 
+  // 1. Create a variable that calls the getRandomQuote()
+  // function
+  
+  let randomQuote = getRandomQuote();
+
+  // 2. Create a variable that initiates your HTML string with
+  // the first two <p></p> elements, their classNames,
+  // and the quote and source properties, but leave off
+  // the second closing `</p>` tag for now
+
+  let PtoHTML1 = "<p class='quote'>" + randomQuote.quote + "</p>"+ "<p class='source'>" + randomQuote.source;
+
+  // 3. Use an if statement to check if the citation property
+  // exists, and if it does, concatenate a <span></span>
+  // element, appropriate className, and citation property
+  // to the HTML string
+
+  if(getRandomQuote().citation){
+    PtoHTML1 += "<span class='citation'>" + getRandomQuote().citation +"</span>";
+  }
+
+  // 4. Use an if statement to check of the year property exists,
+  // and if it does, concatenate a <span></span> element,
+  // appropriate className, and year property to the HTML
+  //string
+
+  if(getRandomQuote().year){
+    PtoHTML1 += "<span class='year'>" + getRandomQuote().year +"</span>";
+  }
+
+  // 5. After the two if statements, concatenate the closing </p>
+  // tag to the HTML string
+
+  PtoHTML1 += "</p>";
+
+  // 6. set the innerHTML of the quote-box div to equal the
+  // complete HTML string}
+
+  document.getElementById("quote-box").innerHTML = PtoHTML1;
+  
+}
 
 
 /***
